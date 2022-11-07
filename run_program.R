af46644faea50f1939feec6b09b7660663dddfa3
Mingkai Jiang <shanyujordan512@gmail.com>
2022-11-07 11:50:04 +0800

    fertilizer treatment added

--- run_program.R
+++ run_program.R
@@ -17,7 +17,7 @@ source("prepare.R")
 ### read input data on LMA
 lmaDF <- read.csv("data/ChangXing_data_LMA_20221107.csv", header=T)
 
-sumDF <- summaryBy(LMA_g_cm2~Species+Harvest_date, data=lmaDF, FUN=c(mean,sd),
+sumDF <- summaryBy(LMA_g_cm2~Species+Harvest_date+Fertilizer_treatment, data=lmaDF, FUN=c(mean,sd),
                    na.rm=T, keep.names=T)
 
 
@@ -28,7 +28,7 @@ p1 <- ggplot(sumDF, aes(x=Species, y=LMA_g_cm2.mean, group=Species))+
                   position=position_dodge(width=0.5), width=0.2) +
     geom_point(data=sumDF, aes(Species, LMA_g_cm2.mean, fill=Species),
                size=3, pch=21, position=position_dodge(width=0.5))+
-    facet_wrap(. ~ Harvest_date, scales="free") +
+    facet_wrap(. ~ Harvest_date+Fertilizer_treatment, scales="free") +
     theme_linedraw() +
     theme(panel.grid.minor=element_blank(),
           axis.text.x=element_text(size=12),
@@ -56,7 +56,8 @@ biomassDF <- read.csv("data/ChangXing_data_Biomass_20221107.csv", header=T)
 
 biomassDF$Total <- with(biomassDF, (Leaf_biomass_g_DM+Stem_biomass_g_DM+Root_biomass_g_DM))
 
-sumDF <- summaryBy(Leaf_biomass_g_DM+Stem_biomass_g_DM+Root_biomass_g_DM+Total~Species+Harvest_date, data=biomassDF, FUN=c(mean,sd),
+sumDF <- summaryBy(Leaf_biomass_g_DM+Stem_biomass_g_DM+Root_biomass_g_DM+Total~Species+Harvest_date+Fertilizer_treatment, 
+                   data=biomassDF, FUN=c(mean,sd),
                    na.rm=T, keep.names=T)
 
 
@@ -67,7 +68,7 @@ p2 <- ggplot(sumDF, aes(x=Species, y=Total.mean, group=Species))+
                   position=position_dodge(width=0.5), width=0.2) +
     geom_point(data=sumDF, aes(Species, Total.mean, fill=Species),
                size=3, pch=21, position=position_dodge(width=0.5))+
-    facet_wrap(. ~ Harvest_date, scales="free") +
+    facet_wrap(. ~ Harvest_date+Fertilizer_treatment, scales="free") +
     theme_linedraw() +
     theme(panel.grid.minor=element_blank(),
           axis.text.x=element_text(size=12),
@@ -84,5 +85,95 @@ p2 <- ggplot(sumDF, aes(x=Species, y=Total.mean, group=Species))+
           plot.title = element_text(size=14, face="bold.italic", 
                                     hjust = 0.5))+
     ylab(expression(paste("Total biomass (g DM " * plant^-1 * ")")))+
-    ylim(0, 15)+
+    ylim(0, 12)+
     xlab(expression(paste("Species"))); p2
+
+
+p3 <- ggplot(sumDF, aes(x=Species, y=Leaf_biomass_g_DM.mean, group=Species))+
+    geom_errorbar(mapping = aes(x=Species, ymin=Leaf_biomass_g_DM.mean-Leaf_biomass_g_DM.sd, 
+                                ymax=Leaf_biomass_g_DM.mean+Leaf_biomass_g_DM.sd, col=Species), 
+                  position=position_dodge(width=0.5), width=0.2) +
+    geom_point(data=sumDF, aes(Species, Leaf_biomass_g_DM.mean, fill=Species),
+               size=3, pch=21, position=position_dodge(width=0.5))+
+    facet_wrap(. ~ Harvest_date+Fertilizer_treatment, scales="free") +
+    theme_linedraw() +
+    theme(panel.grid.minor=element_blank(),
+          axis.text.x=element_text(size=12),
+          axis.title.x=element_blank(),
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          panel.grid.major=element_blank(),
+          legend.position="bottom",
+          legend.box = 'horizontal',
+          legend.box.just = 'left',
+          legend.key.width = unit(1.5,"cm"),
+          plot.title = element_text(size=14, face="bold.italic", 
+                                    hjust = 0.5))+
+    ylab(expression(paste("Leaf biomass (g DM " * plant^-1 * ")")))+
+    ylim(0, 7)+
+    xlab(expression(paste("Species"))); p3
+
+
+p4 <- ggplot(sumDF, aes(x=Species, y=Stem_biomass_g_DM.mean, group=Species))+
+    geom_errorbar(mapping = aes(x=Species, ymin=Stem_biomass_g_DM.mean-Stem_biomass_g_DM.sd, 
+                                ymax=Stem_biomass_g_DM.mean+Stem_biomass_g_DM.sd, col=Species), 
+                  position=position_dodge(width=0.5), width=0.2) +
+    geom_point(data=sumDF, aes(Species, Stem_biomass_g_DM.mean, fill=Species),
+               size=3, pch=21, position=position_dodge(width=0.5))+
+    facet_wrap(. ~ Harvest_date+Fertilizer_treatment, scales="free") +
+    theme_linedraw() +
+    theme(panel.grid.minor=element_blank(),
+          axis.text.x=element_text(size=12),
+          axis.title.x=element_blank(),
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          panel.grid.major=element_blank(),
+          legend.position="bottom",
+          legend.box = 'horizontal',
+          legend.box.just = 'left',
+          legend.key.width = unit(1.5,"cm"),
+          plot.title = element_text(size=14, face="bold.italic", 
+                                    hjust = 0.5))+
+    ylab(expression(paste("Stem biomass (g DM " * plant^-1 * ")")))+
+    ylim(0, 4)+
+    xlab(expression(paste("Species"))); p4
+
+
+p5 <- ggplot(sumDF, aes(x=Species, y=Root_biomass_g_DM.mean, group=Species))+
+    geom_errorbar(mapping = aes(x=Species, ymin=Root_biomass_g_DM.mean-Root_biomass_g_DM.sd, 
+                                ymax=Root_biomass_g_DM.mean+Root_biomass_g_DM.sd, col=Species), 
+                  position=position_dodge(width=0.5), width=0.2) +
+    geom_point(data=sumDF, aes(Species, Root_biomass_g_DM.mean, fill=Species),
+               size=3, pch=21, position=position_dodge(width=0.5))+
+    facet_wrap(. ~ Harvest_date+Fertilizer_treatment, scales="free") +
+    theme_linedraw() +
+    theme(panel.grid.minor=element_blank(),
+          axis.text.x=element_text(size=12),
+          axis.title.x=element_blank(),
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_text(size=14),
+          legend.text=element_text(size=12),
+          legend.title=element_text(size=14),
+          panel.grid.major=element_blank(),
+          legend.position="bottom",
+          legend.box = 'horizontal',
+          legend.box.just = 'left',
+          legend.key.width = unit(1.5,"cm"),
+          plot.title = element_text(size=14, face="bold.italic", 
+                                    hjust = 0.5))+
+    ylab(expression(paste("Root biomass (g DM " * plant^-1 * ")")))+
+    ylim(0, 2)+
+    xlab(expression(paste("Species"))); p5
+
+
+
+pdf("output/results_on_20221107.pdf")
+for (i in c(1:5)) {
+    plot(get(paste0("p",i)))
+}
+
+dev.off()
